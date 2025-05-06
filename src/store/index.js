c788ffdf2bf2e0db0e1f6753378807c7e63158bb
Alozi <alozimozi@gmail.com>
2025-05-06 15:02:05 +0300

    Rewrite store from redux to redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,42 +1,66 @@
-import { createStore } from 'redux'
+// import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = {
     counter: 0,
     showCounter: true
 }
 
-function counterReducer(state = initialState, action) {
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialState,
+    reducers: {
+        decrement(state) {
+            state.counter--;
+        },
+        increment(state) {
+            state.counter++;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.amount;
+        },
+        toogle(state) {
+            state.showCounter = !state.showCounter;
         }
     }
+})
 
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        }
-    }
+// function counterReducer(state = initialState, action) {
+//     if (action.type === 'decrement') {
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter
+//         }
+//     }
 
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        }
-    }
+//     if (action.type === 'increment') {
+//         return {
+//             counter: state.counter + 1,
+//             showCounter: state.showCounter
+//         }
+//     }
 
-    if (action.type === 'toogle') {
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter
-        }
-    }
+//     if (action.type === 'increase') {
+//         return {
+//             counter: state.counter + action.amount,
+//             showCounter: state.showCounter
+//         }
+//     }
 
-    return state;
-}
+//     if (action.type === 'toogle') {
+//         return {
+//             showCounter: !state.showCounter,
+//             counter: state.counter
+//         }
+//     }
 
-const store = createStore(counterReducer);
+//     return state;
+// }
+
+// const store = createStore(counterReducer);
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
 
 export default store;
